7dcddd22c7ac89c0334c564afbab95e6418efafe
Mosfik <mdmosfikurrahman.cse@gmail.com>
2023-12-09 23:49:06 +0600

    Initial commit

---
+++ src/test/java/mmr/epde/StudentHub/StudentHubApplicationTests.java
@@ -0,0 +1,13 @@
+package mmr.epde.StudentHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
